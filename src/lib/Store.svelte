a18d02f65dd63818823077c4eb40edf8328de717
Vadim <tolkott35@gmail.com>
2025-06-21 16:08:29 +0500

    Store v2 добавленны стили, обновлен скрипт сохранения localStore, добавлены элементы карточек: accuracy, speed, time

--- src/lib/Store.svelte
+++ src/lib/Store.svelte
@@ -1,27 +1,37 @@
 <script lang="ts">
     import Button from "$lib/Button.svelte"
+    import { parse } from "svelte/compiler";
 
     let {
         show = false,
     } = $props();
 
-    let textArray = $state("Content");
+    let textArray = $state("");
     let nameArray = $state("");
     let arrays    = $state([""]);
 
     function addArray() {
-        localStorage.setItem(nameArray, JSON.stringify(textArray));
-        console.log(localStorage.getItem("test2"));
+        let content = {
+            text: textArray,
+            accuracy: "99",
+            speed: "224",
+        }
+        localStorage.setItem(nameArray, JSON.stringify(content));
         Arrays();
     }
 
     function Arrays() {
         arrays = [];
         for (let i = 0 ; i < localStorage.length ; i++) {
-            console.log(localStorage.key(i));
-            let array = localStorage.key(i);
-            if (array) {
-                arrays.push(array);
+            const key  = localStorage.key(i);
+            const item = key ? localStorage.getItem(key) : null;
+            try {
+                const parsed = item ? JSON.parse(item) : null;
+                if (key && parse !== null) {
+                    arrays.push({name: key, ...parsed});
+                }
+            } catch {
+                alert("error");
             }
         }
     }
@@ -34,19 +44,24 @@
     Arrays(); // Отрисовываем по умолчанию
 </script>
 
-<div class="flex flex-col m-auto w-[50vw]">
-    <input bind:value={nameArray} type="text" placeholder="nameArray">
-    <textarea bind:value={textArray} name="" id="" placeholder="Content"></textarea>
-    <div>
+<div class="flex flex-col m-auto w-[50vw] bg-red-400 p-4 gap-4">
+    <div class="flex flex-col p-4 rounded-xl bg-white/15 border-1 border-white/25 backdrop-blur-sm shadow-lg">
+        <input bind:value={nameArray} type="text"  placeholder="nameArray" class="border-b-1 border-white/25 pb-4 text-white font-bold text-[20px] ">
+        <textarea bind:value={textArray} name="" id="" placeholder="Content" class="text-white mt-4 font-bold resize-none"></textarea>
         <Button onclick={addArray}>addArray</Button>
-        <div>
-            {#each arrays as array}
-                <div>
-                    <p>{array}</p>
-                    <Button onclick={() => delArray(array)}>delArray</Button>
-                    <Button>mixArray</Button>
+    </div>
+    <div class="flex flex-wrap gap-4">
+        {#each arrays as item}
+            <div class="flex flex-col text-white p-4 rounded-xl bg-white/15 border-1 border-white/25 backdrop-blur-sm w-50 gap-2 shadow-lg">
+                <h1 class="font-bold text-[20px] border-white/25 border-b-1 pb-3">{item.name}</h1>
+                <p>accuracy: {item.accuracy}</p>
+                <p>speed: {item.speed}</p>
+                <p>time: </p>
+                <div class="flex gap-2">
+                    <Button onclick={() => delArray(item.name)}>delArray</Button>
+                    <Button>...</Button>
                 </div>
-            {/each}
-        </div>
+            </div>
+        {/each}
     </div>
 </div>
